41281a11aa7b99468e1c2c02c724c9eb68aa0596
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-18 11:27:56 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener('submit', (event) => {
+    event.preventDefault()
+    let task = event.target
+    addTask(task)
+  })
+
 });
+
+function addTask(formData) {
+
+  let taskList = document.getElementById("tasks")
+
+  let taskBlock = document.createElement('li')
+    taskBlock.innerText = formData.newTaskDescription.value
+    if (formData.taskPriority.value == "high") {
+      taskBlock.style = "color:red"
+      taskBlock.class = "highTask"
+    } else if (formData.taskPriority.value == "medium") {
+      taskBlock.style = "color:yellow"
+      taskBlock.class = "mediumTask"
+    } else if (formData.taskPriority.value == "low") {
+      taskBlock.style = "color:green"
+      taskBlock.class = "lowTask"
+    }
+
+  let deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "Delete"
+    deleteBtn.addEventListener('click', () => {
+      deleteBtn.parentNode.remove()
+    })
+
+  taskBlock.appendChild(deleteBtn)
+  taskList.appendChild(taskBlock)
+}
